ef785289cfaedc5ec5c10c20b8df52f8dc93436b
sameerthor <sameer@thor.solutions>
2024-06-21 20:15:06 +0530

    latest push

--- src/components/coupon.js
+++ src/components/coupon.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 export default function Coupon({ index_id, store_data, coupon_data }) {
     const [couponShow, setCouponShow] = useState(false);
+    const [feedbackShow, setFeedbackShow] = useState(false);
+    const [feedbackValue, setFeedbackValue] = useState(false);
     const [copyTextShow, setCopyTextShow] = useState(false);
     const [dealTextShow, setDealTextShow] = useState(false);
 
@@ -11,15 +13,25 @@ export default function Coupon({ index_id, store_data, coupon_data }) {
             var c_id = localStorage.getItem("copied_code")
             if (c_id) {
                 setCouponShow(true)
+
             }
+
             if (c_id == coupon_data.id) {
                 setCopyTextShow(true);
+                setFeedbackShow(true)
                 setTimeout(() => {
                     localStorage.removeItem("copied_code");
 
                 }, 2000)
-
+               
             }
+            var feedback = localStorage.getItem("feedback_" + coupon_data.id)
+            if(feedback)
+            {
+                setFeedbackShow(true)
+
+            }    
+            setFeedbackValue(feedback)
         }
     }, 1000)
 
@@ -65,7 +77,7 @@ export default function Coupon({ index_id, store_data, coupon_data }) {
                             setDealTextShow(true)
                             setTimeout(() => {
                                 setDealTextShow(false)
-                                ; window.open(store_data.affiliate_url, "_blank");
+                                    ; window.open(store_data.affiliate_url, "_blank");
                             }, 1500)
 
 
@@ -78,24 +90,32 @@ export default function Coupon({ index_id, store_data, coupon_data }) {
 
 
                 </div>
-                <div className='feedBackBox d-none'>
+                {feedbackShow && <div className='feedBackBox'>
                     <div className='feedbackDetails'>
                         <div className='feedBacktext'>
-                            <p>Did this Coupon work?</p>
-                            <p className='d-none'>Thank you for your feedback 🙏</p>
+                            {feedbackValue == "1" || feedbackValue == "2" ? <p>Thank you for your feedback 🙏</p>
+                                : <p>Did this Coupon work?</p>}
+
                         </div>
                         <div className='feedBackBtns'>
-                            <button>
-                                <svg id="thumbsdownsvg" class="cursor-pointer stroke-current text-red-500 stroke-1 mx-1" width="18" height="16" viewBox="0 0 18 16" xmlns="http://www.w3.org/2000/svg" fill="white">
+                            <button type='button' className={feedbackValue=="2" && "selected"}   disabled={(feedbackValue=="1" || feedbackValue=="2") && true  } onClick={(e) => {
+                                localStorage.setItem("feedback_" + coupon_data.id, "2")
+                                setFeedbackValue("2")
+                            }}>
+                               <svg id="thumbsdownsvg" class="cursor-pointer stroke-current text-red-500 stroke-1 mx-1" width="18" height="16" viewBox="0 0 18 16" xmlns="http://www.w3.org/2000/svg" fill="white">
                                     <path d="M11.25 0.25H4.5C3.8775 0.25 3.345 0.625 3.12 1.165L0.855 6.4525C0.7875 6.625 0.75 6.805 0.75 7V8.5C0.75 9.325 1.425 10 2.25 10H6.9825L6.27 13.4275L6.2475 13.6675C6.2475 13.975 6.375 14.26 6.5775 14.4625L7.3725 15.25L12.315 10.3075C12.585 10.0375 12.75 9.6625 12.75 9.25V1.75C12.75 0.925 12.075 0.25 11.25 0.25ZM14.25 0.25V9.25H17.25V0.25H14.25Z" stroke=""></path>
                                 </svg>
-                                 No
+                                No
                             </button>
-                            <button>
-                            <svg id="thumbsupsvg" class="cursor-pointer stroke-current text-green-500 stroke-1 mx-1" width="18" height="16" viewBox="0 0 18 16" xmlns="http://www.w3.org/2000/svg" fill="white">
-                                <path d="M0.75 15.75H3.75V6.75H0.75V15.75ZM17.25 7.5C17.25 6.675 16.575 6 15.75 6H11.0175L11.73 2.5725L11.7525 2.3325C11.7525 2.025 11.625 1.74 11.4225 1.5375L10.6275 0.75L5.6925 5.6925C5.415 5.9625 5.25 6.3375 5.25 6.75V14.25C5.25 15.075 5.925 15.75 6.75 15.75H13.5C14.1225 15.75 14.655 15.375 14.88 14.835L17.145 9.5475C17.2125 9.375 17.25 9.195 17.25 9V7.5Z" stroke=""></path>
-                            </svg>
-                                 Yes
+                            <button type="button" className={feedbackValue=="1" && "selected"} disabled={(feedbackValue=="1" || feedbackValue=="2") && true  } onClick={(e) => {
+                                localStorage.setItem("feedback_" + coupon_data.id, "1")
+                                setFeedbackValue("1")
+                            }}>
+                                
+                                <svg id="thumbsupsvg" class="cursor-pointer stroke-current text-green-500 stroke-1 mx-1" width="18" height="16" viewBox="0 0 18 16" xmlns="http://www.w3.org/2000/svg" fill="white">
+                                    <path d="M0.75 15.75H3.75V6.75H0.75V15.75ZM17.25 7.5C17.25 6.675 16.575 6 15.75 6H11.0175L11.73 2.5725L11.7525 2.3325C11.7525 2.025 11.625 1.74 11.4225 1.5375L10.6275 0.75L5.6925 5.6925C5.415 5.9625 5.25 6.3375 5.25 6.75V14.25C5.25 15.075 5.925 15.75 6.75 15.75H13.5C14.1225 15.75 14.655 15.375 14.88 14.835L17.145 9.5475C17.2125 9.375 17.25 9.195 17.25 9V7.5Z" stroke=""></path>
+                                </svg>
+                                Yes
                             </button>
                         </div>
                         <div className='feedBacktext'>
@@ -104,7 +124,8 @@ export default function Coupon({ index_id, store_data, coupon_data }) {
                             </p>
                         </div>
                     </div>
-                </div>
+                </div>}
+
             </div>
         </>
 
