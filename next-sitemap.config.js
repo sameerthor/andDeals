7bf80b35870d6cae9659229e24525ce5b659dd36
sameerthor <sameer@thor.solutions>
2024-06-25 12:54:53 +0530

    remove index sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,6 @@ module.exports = {
     siteUrl:'https://www.anddeals.com',
     generateRobotsTxt: true, // (optional)
     sitemapBaseFileName:"anddeals-sitemap",
+    generateIndexSitemap: false,
     sitemapSize: 7000
   }
