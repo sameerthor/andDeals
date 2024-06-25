fd22db7c53c9ec6fff79fad99f69e829d11105d4
sameerthor <sameer@thor.solutions>
2024-06-25 12:46:32 +0530

    sitemap data

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl:'https://www.anddeals.com',
+    generateRobotsTxt: true, // (optional)
+    sitemapBaseFileName:"anddeals-sitemap",
+    sitemapSize: 7000
+  }
