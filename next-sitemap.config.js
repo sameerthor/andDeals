eebe03e24a0e008dad09659933baec7856893abe
sameerthor <sameer@thor.solutions>
2024-06-25 13:24:23 +0530

    sitemap date

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,5 +4,14 @@ module.exports = {
     generateRobotsTxt: true, // (optional)
     sitemapBaseFileName:"anddeals-sitemap",
     generateIndexSitemap: false,
-    sitemapSize: 7000
+    sitemapSize: 7000,
+    transform: async (config, path) => {
+        return {
+          loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+          changefreq: config.changefreq,
+          priority: config.priority,
+          lastmod: config.autoLastmod ? new Date().toISOString().split('T')[0] : undefined,
+          alternateRefs: config.alternateRefs ?? [],
+        }
+      }
   }
