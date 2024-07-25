addf1a91493a3f533f5773b4e2168a790a809a1b
sameerthor <sameer@thor.solutions>
2024-07-25 12:54:48 +0530

    sitemap issue

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -7,7 +7,7 @@ module.exports = {
         return {
             loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
             changefreq: "monthly",
-            lastmod: config.autoLastmod ? new Date().getFullYear() + "-" + String("0" + parseInt(new Date().getMonth()) + 1).slice(-2) + "-01" + "T00:00:00-05:00" : undefined,
+            lastmod: config.autoLastmod ? new Date().getFullYear() + "-" + String("0" + (parseInt(new Date().getMonth()) + 1)).slice(-2) + "-01" + "T00:00:00-05:00" : undefined,
         }
     }
 }
